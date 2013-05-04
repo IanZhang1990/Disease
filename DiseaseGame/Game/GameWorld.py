83420bb59b514ae13ffec7cac68a17c69d9aefa6
Yinan Zhang <ian.zhang1990@hotmail.com>
2013-05-04 12:07:06 +0800

    Correct a mistake

--- DiseaseGame/Game/GameWorld.py
+++ DiseaseGame/Game/GameWorld.py
@@ -7,6 +7,14 @@
 #=====================================================================================
 
 from GUI.DisplayScreen import DisplayScreen
+from Utils.Path import Path
+from Game.Man import ManParamLoader
+from Game import Man
+from Utils.SteeringBehavior import SteeringBehavior
+from Utils.SteeringBehavior import SteeringParmLoader
+from Math.Vector import Vector2D
+import math
+from random import random
 
 class GameWorld(object):
     """GameWorld class controls all the basic operations of a game, 
@@ -24,20 +32,37 @@ class GameWorld(object):
     Doctors = list()                     # a container of all doctors
     Cities = list()                        # a container of all cities
 
-    ManPath = Null                     # any path we may create for the men to follow
+    ManPath = None                     # any path we may create for the men to follow
 
     def __init__(self):
-        pass
+
+        border = 30;
+        self.ManPath = Path( 8, border, border, self.WorldWidth-border, self.WorldHeight-border, True)
+
+        # Set up cities
+
+        # Set up roads
+
+        # Set up people 
+        manParmLoader = ManParamLoader( "Man.man");
+        agentNumber = int(manParmLoader.Parameters.get('AgentNumber', 40))
+        steeringParmLoader = SteeringParmLoader( "Steering.str")
+
+        for i in range(0, agentNumber):
+            # determine a random starting position
+            pos = Vector2D( random.uniform(0, self.WorldWidth), random.uniform( 0, self.WorldHeight))
+            newPerson = Man.Man( self, manParmLoader, steeringParmLoader, True, 20,  pos, Vector2D(0, 0))
+            self.People.append( newPerson )
 
     def Render(self):
         """Render objects to the screen"""
         if not self.Pause:
             for person in self.People:
-                person.Update( elapsedTime )
+                person.Render()
             for doctor in self.Doctors:
-                doctor.Update( elapsedTime )
+                doctor.Render()
             for city in self.Cities:
-                city.Update( elapsedTime )
+                city.Render()
 
             # Render everything
             raise Exception();
